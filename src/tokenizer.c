6321f0e675f4c7d90b692500edec07febacae857
Khdbble <fb503d52.af1c4d@gmail.com>
2020-06-20 14:46:43 +0900

    added bitshifts (<<, >>, <<=, >>=)

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -368,7 +368,14 @@ static Token *tokenize(char *filename, char *p) {
             continue;
         }
 
-        // Multi-letter punctuators
+        // Three-letter punctuators
+        if (startswith(p, "<<=") || startswith(p, ">>=")) {
+            cur = new_token(TK_RESERVED, cur, p, 3);
+            p += 3;
+            continue;
+        }
+
+        // Two-letter punctuators
         if (startswith(p, "==") || startswith(p, "!=") ||
             startswith(p, "<=") || startswith(p, ">=") ||
             startswith(p, "->") || startswith(p, "+=") ||
@@ -377,7 +384,8 @@ static Token *tokenize(char *filename, char *p) {
             startswith(p, "--") || startswith(p, "%=") ||
             startswith(p, "&=") || startswith(p, "|=") ||
             startswith(p, "^=") || startswith(p, "&&") ||
-            startswith(p, "||")) {
+            startswith(p, "||") || startswith(p, "<<") ||
+            startswith(p, ">>")) {
             cur = new_token(TK_RESERVED, cur, p, 2);
             p += 2;
             continue;
