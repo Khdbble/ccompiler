736c86f48e2da740a3c8b8e0f9cca07b3fd1634e
Khdbble <fb503d52.af1c4d@gmail.com>
2020-06-21 15:44:08 +0900

    allow parentheses in initializers to be omitted

--- src/parser.c
+++ src/parser.c
@@ -657,8 +657,9 @@ static Initializer *string_initializer(Token **rest, Token *tok, Type *ty) {
 }
 
 // array-initializer = "{" initializer ("," initializer)* "}"
+//                   | initializer ("," initializer)* ","
 static Initializer *array_initializer(Token **rest, Token *tok, Type *ty) {
-    tok = skip(tok, "{");
+    bool has_paren = consume(&tok, tok, "{");
 
     if (ty->is_incomplete) {
         int i = 0;
@@ -680,11 +681,14 @@ static Initializer *array_initializer(Token **rest, Token *tok, Type *ty) {
             tok = skip(tok, ",");
         init->children[i] = initializer(&tok, tok, ty->base);
     }
-    *rest = skip_end(tok);
+    if (has_paren)
+        tok = skip_end(tok);
+    *rest = tok;
     return init;
 }
 
 // struct-initializer = "{" initializer ("," initializer)* "}"
+//                    | initializer ("," initializer)* ","
 static Initializer *struct_initializer(Token **rest, Token *tok, Type *ty) {
     if (!equal(tok, "{")) {
         Token *tok2;
@@ -702,7 +706,7 @@ static Initializer *struct_initializer(Token **rest, Token *tok, Type *ty) {
         len++;
 
     Initializer *init = new_init(ty, len, NULL, tok);
-    tok = skip(tok, "{");
+    bool has_paren = consume(&tok, tok, "{");
 
     int i = 0;
     for (Member *mem = ty->members; mem && !equal(tok, "}"); mem = mem->next, i++) {
@@ -710,7 +714,10 @@ static Initializer *struct_initializer(Token **rest, Token *tok, Type *ty) {
             tok = skip(tok, ",");
         init->children[i] = initializer(&tok, tok, mem->ty);
     }
-    *rest = skip_end(tok);
+
+    if (has_paren)
+        tok = skip_end(tok);
+    *rest = tok;
     return init;
 }
 
