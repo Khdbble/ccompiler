50826dba8b85bf2b67abb27a2ae90dea1f008990
Khdbble <fb503d52.af1c4d@gmail.com>
2020-06-21 15:37:37 +0900

    allow global variable initializer to have an addend

--- src/nsc.h
+++ src/nsc.h
@@ -11,6 +11,7 @@
 
 typedef struct Type Type;
 typedef struct Member Member;
+typedef struct Relocation Relocation;
 
 //
 // tokenize.c
@@ -65,6 +66,18 @@ struct Var {
 
     // Global variable
     char *init_data;
+    Relocation *rel;
+};
+
+// Global variable can be initialized either by a constant expression
+// or a pointer to another global variable. This struct represents the
+// latter.
+typedef struct Relocation Relocation;
+struct Relocation {
+    Relocation *next;
+    int offset;
+    char *label;
+    long addend;
 };
 
 // AST node
