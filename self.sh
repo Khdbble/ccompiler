33c364bcdc034f8bdb6213ef0bc100af815aeacc
Khdbble <fb503d52.af1c4d@gmail.com>
2020-06-22 10:59:16 +0900

    added __builtin_va_start to support variadic function

--- self.sh
+++ self.sh
@@ -11,6 +11,14 @@ typedef struct FILE FILE;
 extern FILE *stdin;
 extern FILE *stdout;
 extern FILE *stderr;
+
+typedef struct {
+  int gp_offset;
+  int fp_offset;
+  void *overflow_arg_area;
+  void *reg_save_area;
+} va_list[1];
+
 void *malloc(long size);
 void *calloc(long nmemb, long size);
 void *realloc(void *buf, long size);
@@ -30,6 +38,7 @@ void *memcpy(char *dst, char *src, long n);
 char *strndup(char *p, long n);
 int isspace(int c);
 char *strstr(char *haystack, char *needle);
+static void va_end(va_list ap) {}
 EOF
 
     grep -v '^#' nsc.h >> $TMP/$1
@@ -39,6 +48,7 @@ EOF
     sed -i 's/\btrue\b/1/g; s/\bfalse\b/0/g;' $TMP/$1
     sed -i 's/\bNULL\b/0/g' $TMP/$1
     sed -i 's/INT_MAX/2147483647/g' $TMP/$1
+    sed -i 's/\bva_start\b/__builtin_va_start/g' $TMP/$1
 
     ./nsc $TMP/$1 > $TMP/${1%.c}.s
     gcc -c -o $TMP/${1%.c}.o $TMP/${1%.c}.s
